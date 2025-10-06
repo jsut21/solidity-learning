27d4c6fb0d65096b8f8f43c04c7875aaa76be094
jsut21 <zzs6348@gmail.com>
2025-10-06 15:50:21 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,15 +9,27 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _deicimal) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _deicimal,
+        uint256 _amount
+    ) {
         name = _name;
         symbol = _symbol;
         decimals = _deicimal;
-        _mint(1 * 10 ** uint256(decimals), msg.sender);
+        _mint(_amount * 10 ** uint256(decimals), msg.sender);
     }
 
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
+    }
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
     }
 }
