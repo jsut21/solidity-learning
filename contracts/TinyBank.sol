1b3e88ae25fbdaf20e0d22831309825c2c408eb5
jsut21 <zzs6348@gmail.com>
2025-10-28 00:01:47 +0900

    access manager apply to TinyBank

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import "./ManagedAccess.sol";
+
 interface IMyToken {
     function transfer(uint256 amount, address to) external;
 
@@ -9,19 +11,22 @@ interface IMyToken {
     function mint(uint256 amount, address owner) external;
 }
 
-contract TinyBank {
+contract TinyBank is ManagedAccess {
     event Staked(address, uint256);
     event Withdraw(uint256 amount, address to);
 
     mapping(address => uint256) public lastClaimedBlock;
-    uint256 rewardPerBlock = 1 * 10 ** 18;
+
+    uint256 defaultRewardPerBlock = 1 * 10 ** 18;
+    uint256 rewardPerBlock;
 
     IMyToken public stakingToken;
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
 
-    constructor(IMyToken _stakingToken) {
+    constructor(IMyToken _stakingToken) ManagedAccess(msg.sender, msg.sender) {
         stakingToken = _stakingToken;
+        rewardPerBlock = defaultRewardPerBlock;
     }
 
     modifier updateReward(address to) {
@@ -33,6 +38,10 @@ contract TinyBank {
         }
         lastClaimedBlock[to] = block.number;
         _;
+    }
+
+    function setRewardPerBlock(uint256 _amount) external onlyManager {
+        rewardPerBlock = _amount;
     }
 
     function stake(uint256 _amount) external updateReward(msg.sender) {
